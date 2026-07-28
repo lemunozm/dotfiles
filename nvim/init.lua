a7157107039ce6d5777866b0d2a33aa7d403d1f4
lemunozm <lemunozm@gmail.com>
2026-07-28 09:28:13 +0200

    fix markdown plugin installation

--- nvim/init.lua
+++ nvim/init.lua
@@ -351,7 +351,7 @@ require("lazy").setup(
       "iamcco/markdown-preview.nvim",
       ft = { "markdown" },
       cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-      build = "cd app && yarn install",
+      build = function() vim.fn["mkdp#util#install"]() end,
       init = function()
         vim.g.mkdp_filetypes = { "markdown" }
       end,
